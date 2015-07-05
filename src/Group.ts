1fa20abb04cff9056efbf1f8e58e0c8f438ade67
Sergey Romanov <xxsmotur@gmail.com>
2015-07-05 21:40:53 +0500

    New feature: Symmetric group

--- src/Group.ts
+++ src/Group.ts
@@ -1,3 +1,5 @@
+import Abstract = require('./Abstract');
+import permutations = require('./Permutations');
 
 //Definition of group
 export class Group{
@@ -29,3 +31,36 @@ export class ConstantGroup{
         return this.element;
     }
 }
+
+//Construct symmetric group
+/*Symmetric group
+degree provides number of permutations with degree!
+For example if degree = 4, this is S4 group
+http://groupprops.subwiki.org/wiki/Symmetric_group:S4
+*/
+export class SymmetricGroup{
+    private degree: number;
+    private elements: number[][];
+    private ordernum: number
+
+    private constructElements(degree: number): number[][] {
+        var list:Array<number> = [];
+        for(let i = 0;i < degree;++i){
+            list.push(i);
+        }
+
+        let perm = new permutations.Permutations<number>(list);
+        this.ordernum = perm.total_num_of_permutations();
+        return perm.all_permutations();
+    }
+    constructor(degree: number) {
+        this.degree = degree;
+        //console.log(new Permutations.Permutations([1,2,3]));
+        this.elements = this.constructElements(degree);
+    }
+
+    order(): number {
+        return this.ordernum;
+    }
+}
+
